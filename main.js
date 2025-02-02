f01525d66cc2061f4bf644ff64f8eb424015518e
Noor Halabi <halabinoor18@gmail.com>
2025-02-02 20:53:30 +0200

    Update main.js

--- main.js
+++ main.js
@@ -1,28 +1,25 @@
-let sumb = document.getElementById("submit")
-let jwtToken;
-
-sumb.addEventListener("click", () => {
+document.getElementById("submit").addEventListener("click", () => {
     const username = document.getElementById("user").value
     const password = document.getElementById("pass").value
-    const credentials = btoa(`${username}:${password}`); // Encode username and password in base 64
-    console.log(username)
-    console.log(password)
-    fetch('https://adam-jerusalem.nd.edu/api/auth/signin', { // sent request to get information
-            method: 'POST', // bc we are sending info 
-            headers: { // for additionl info
+    const credentials = btoa(`${username}:${password}`); // base 64 encoded string
+    // console.log(username)
+    // console.log(password)
+    fetch('https://adam-jerusalem.nd.edu/api/auth/signin', { 
+            method: 'POST',
+            headers: { 
                 'Authorization': `Basic ${credentials}`
             }
         })
-        .then(response => { // confirms the request is success
+        .then(response => { 
             if (!response.ok) {
                 document.getElementById('errorm').style.display = "block"
-                throw new Error('Invalid username or password'); // Invalid credentials
+                throw new Error('Invalid username or password'); 
+                // throws err in console & stops further execution
             }
-            return response.json(); // Converts the info we got into a JSON object for further use.
+            return response.json();
         })
-        .then(data => { // deal with the jason we got from .then respone
-            jwtToken = data;
-            localStorage.setItem('jwtToken', jwtToken); // saves
+        .then(data => { // deal with the json we got from prev .then 
+            localStorage.setItem('jwtToken', data);
             window.location.href = "profile.html";
             console.log('Login successful:', data); // Server sends back JWT(adam info) token
         })
